9a796a89e8207c70188dd4701b26cbe1fcf96eb3
sensiz <dev.federicosensi@gmail.com>
2022-10-26 13:56:04 +0200

    final commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,29 @@
 
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
 // Bonus:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
+const {createApp} = Vue 
 
-
-
+createApp({
+    data(){
+        return{
+            title: 'Hello World!',
+            titleColor: 'color',
+            button: 'btn btn-danger'
+        }
+    },
+    methods: {
+        changer(){
+            if(this.titleColor == 'color' && this.button == 'btn btn-danger'){
+                this.titleColor = 'colorChanged'
+                this.button = 'btn btn-primary'
+            } else {
+                this.titleColor = 'color'
+                this.button = 'btn btn-danger'
+            }
+        }
+    }
+}).mount('#app')
